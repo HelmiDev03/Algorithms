d38ef00dc67d7569c4e09b739972b6c85e4cf1b8
Helmi Lakhder <102436033+HelmiDev03@users.noreply.github.com>
2023-05-31 23:36:39 +0100

    Update ProblemC.py

--- Competitive_Programming /TCPC2022/ProblemC.py
+++ Competitive_Programming /TCPC2022/ProblemC.py
@@ -1,9 +1,13 @@
 def next(x):
-    if ( x %2 ):
-        return 1
-    else:
-        return x // 2 
+    divisor = x // 2  # Start with the largest possible divisor
     
+    # Iterate from x//2 down to 1
+    while divisor > 1:
+        if x % divisor == 0:
+            return divisor
+        divisor -= 1
+    
+    return 1  # Return 1 if no divisor other than x itself is found
 final=[]
 counter=0
 check=False
@@ -12,28 +16,14 @@ for tour in range (t):
     longe=int(input())
     a=list( map(int, input().split()[:longe]))
     b=list( map(int, input().split()[:longe]))
-    for i,indexi in enumerate(a):
-        for j,indexj in enumerate(b):
-            if indexj==indexi:
-                while i!=j :
-                    counter+=1
-                    if (i>j):
-            
-                        st=str(a)
-                        st.replace(str(i),str(next(i)))
-                        a = [int(num) for num in a]
-                        i=a[indexi]
-                
-                    else :
-               
-                        st=str(b)
-                        st.replace(str(j),str(next(j)))
-                        b=[int(num) for num in b]
-                        j=b[indexj]
-                
-            
+    for i,j in zip (a,b):
+        while i!=j :
+            counter+=1
+            if (i<j):
+                i, j = j, i
+            i=next(i)
         
     final.append(counter)
     counter=0
 for index in final:
-    print(index)  
+    print(index) 
